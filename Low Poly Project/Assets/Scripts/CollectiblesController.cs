24c116229813b505fa466a30e332d70d5be5a200
GuitarTom98 <tomudale@googlemail.com>
2018-11-02 18:50:22 +0000

    First commit
    My first commit for the project

---
+++ Low Poly Project/Assets/Scripts/CollectiblesController.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class CollectiblesController : MonoBehaviour {
+
+	public CollectiblesData[] cd;
+
+	void Awake(){
+	
+		DontDestroyOnLoad (gameObject);
+
+		if (FindObjectsOfType (GetType ()).Length > 1) {
+		
+			Destroy (gameObject);
+		
+		}
+	
+	}
+
+
+	void Update(){
+	
+		if (Input.GetKeyDown ("l")) {
+		
+			Debug.Log ("Loading");
+			LoadData ();
+		
+		} else if (Input.GetKeyDown ("s")) {
+		
+			Debug.Log ("Saving");
+			SaveData ();
+		
+		}
+	
+	}
+
+	public void incrementCount(GameObject go)
+	{
+
+		if (go.name.Contains ("Heart")) {
+		
+			cd [0].collectibleNum++;
+		
+		}
+
+		if (go.name.Contains ("Coconut")) {
+
+			cd [1].collectibleNum++;
+
+		}
+
+		if (go.name.Contains ("Diamond")) {
+
+			cd [2].collectibleNum++;
+
+		}
+	
+	}
+
+	void outputCounts()
+	{
+	
+		Debug.Log ("You've collected:");
+		Debug.Log ("Hearts = " + cd [0].collectibleNum);
+		Debug.Log ("Coconuts = " + cd [1].collectibleNum);
+		Debug.Log ("Diamonds = " + cd [2].collectibleNum);
+	
+	}
+
+	public void SaveData(){
+	
+		BinaryFormatter bf = new BinaryFormatter ();
+		FileStream fs = File.Create (Application.persistentDataPath + "/gameData.dat");
+		bf.Serialize (fs, cd);
+		fs.Close ();
+		Debug.Log ("Save Data");
+	
+	}
+
+	public void LoadData(){
+	
+		if (File.Exists (Application.persistentDataPath + "/gameData.dat")) {
+		
+			BinaryFormatter bf = new BinaryFormatter ();
+			FileStream fs = File.Open (Application.persistentDataPath + "/gameData.dat", FileMode.Open);
+			cd = (CollectiblesData[])bf.Deserialize (fs);
+			fs.Close ();
+			Debug.Log ("Loading Data");
+		
+		} else {
+		
+			Debug.LogError ("404");
+		
+		}
+	}
+
+}
